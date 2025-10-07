64b388a7f296c504e895793a396488a114d34d8c
WelinCode <welin1924@gmail.com>
2025-10-07 13:23:21 -0400

    solution

--- frontend/src/app/products/new/page.tsx
+++ frontend/src/app/products/new/page.tsx
@@ -1,6 +1,6 @@
 function ProductsPage() {
   return (
-    <div>pagess</div>
+    <div>New Product</div>
   )
 }
 
--- frontend/src/app/providers/QueryProvider.tsx
+++ frontend/src/app/providers/QueryProvider.tsx
@@ -2,13 +2,24 @@
 
 import { ReactNode, useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 interface QueryProviderProps {
   children: ReactNode
 }
 
 export default function QueryProvider({ children }: QueryProviderProps) {
-  const [queryClient] = useState(() => new QueryClient())
+    const [queryClient] = useState(
+    () =>
+        new QueryClient({
+        defaultOptions: {
+            queries: {
+            staleTime: 30_000,
+            refetchOnWindowFocus: false,
+            },
+        },
+        }),
+    )
 
   return (
     <QueryClientProvider client={queryClient}>
