c4cffa0a6796189314c6fb6113ee49cded3b5e58
WelinCode <welin1924@gmail.com>
2025-10-22 13:23:33 -0400

    images to home pages

--- frontend/src/components/BestSellersSection.tsx
+++ frontend/src/components/BestSellersSection.tsx
@@ -1,21 +1,27 @@
 'use client';
 
 import ProductCard from '@/components/ProductCard';
-import PRODUCTS from '@/data/Products';
+import productsData from '@/data/products.json';
 import { useI18n } from '@/hooks/useI18n';
 
 const BestSellersSection = () => {
   const { t } = useI18n();
 
   return (
-    <section className="flex flex-col items-center px-5 md:px-12 lg:px-16 xl:px-[60px] py-24 gap-16 w-full">
+    <section className="flex flex-col items-center px-5 md:px-12 lg:px-16 xl:px-[60px] py-24 gap-16 w-full overflow-x-hidden">
       <h2 className="font-roboto font-medium text-[32px] leading-[40px] text-center text-black w-full max-w-[400px] md:max-w-none">
         {t('bestsellers.bestSellingProducts')}
       </h2>
 
-      <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 items-center md:items-start gap-12 w-full max-w-[440px] md:max-w-[832px] lg:max-w-[1320px]">
-        {PRODUCTS.map((product) => (
-          <ProductCard key={product.id} title={product.title} description={product.description} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full mx-auto">
+        {productsData.slice(0, 3).map((product) => (
+          <div key={product.id} className="w-full max-w-[400px] mx-auto">
+            <ProductCard
+              title={product.title}
+              description={product.description}
+              imageSrc={product.imageSrc}
+            />
+          </div>
         ))}
       </div>
     </section>
--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,28 +1,29 @@
 import ProductCardProps from "@/interface/ProductCard";
+import Image from "next/image";
 
 function ProductCard({ title, description, imageSrc }: ProductCardProps) {
   return (
     <div
       className="
         flex flex-col justify-center items-start gap-6
-        w-[400px] h-[695px]               /* Tamaño fijo en mobile */
-        md:w-full md:h-auto              /* En tablet se adapta */
-        lg:w-[400px] lg:h-[695px] lg:flex-1
+        w-full max-w-[400px] h-auto
       "
     >
       <div
         className="
-          flex flex-row items-start p-[10px] gap-[10px]
+          flex flex-row items-start gap-[10px]
           w-full h-[611px] bg-[#E8E8E8] rounded-2xl
           md:h-[500px]
           lg:h-[611px]
         "
       >
         {imageSrc && (
-          <img
+          <Image
             src={imageSrc}
             alt={title}
             className="w-full h-full object-cover rounded-2xl"
+            width={400}
+            height={611}
           />
         )}
       </div>
